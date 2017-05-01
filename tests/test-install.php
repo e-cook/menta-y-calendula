7ef0de78a3d0ef4dc408e86d46b88f7da11ac4a3
e-cook <e-cook@>
2017-05-01 17:48:06 +0200

    new table structure

--- tests/test-install.php
+++ tests/test-install.php
@@ -15,18 +15,39 @@ class InstallTest extends WP_UnitTestCase {
 	$this->assertTrue( myc_install() === 1 );
     }
 
-    function test_populate_products() {
+    function test_populate_ingredients() {
 	global $wpdb;
-	$posts_table = $wpdb->prefix . 'posts';
-	populate_products($wpdb, $posts_table);
-	$this->assertTrue( $wpdb->get_var( "SELECT post_title FROM $posts_table WHERE id=6" ) == 'Ull de Molins' );
+	$table = $wpdb->prefix . 'ingredient';
+	populate_ingredients($wpdb, $table);
+	$this->assertTrue( $wpdb->get_var( "SELECT name FROM $table WHERE id=3" ) == 'All' );
+    }
+
+    function test_populate_ingredient_tags() {
+	global $wpdb;
+	$table = $wpdb->prefix . 'ingredient_tag';
+	populate_ingredient_tags($wpdb, $table);
+	$this->assertTrue( $wpdb->get_var( "SELECT for_id FROM $table WHERE id=3" ) == '3' );
     }
 
     function test_populate_providers() {
 	global $wpdb;
-	$provided_by_table = $wpdb->prefix . 'provided_by';
-	populate_providers($wpdb, $provided_by_table);
-	$this->assertTrue( $wpdb->get_var( "SELECT provider_id FROM $provided_by_table WHERE id=6" ) == '7' );
+	$table = $wpdb->prefix . 'provider';
+	populate_providers($wpdb, $table);
+	$this->assertTrue( $wpdb->get_var( "SELECT name FROM $table WHERE id=4" ) == 'Xarxa' );
+    }
+
+    function test_populate_provider_tags() {
+	global $wpdb;
+	$table = $wpdb->prefix . 'provider_tag';
+	populate_provider_tags($wpdb, $table);
+	$this->assertTrue( $wpdb->get_var( "SELECT for_id FROM $table WHERE id=3" ) == '2' );
+    }
+
+    function test_populate_provided_by() {
+	global $wpdb;
+	$table = $wpdb->prefix . 'provided_by';
+	populate_provided_by($wpdb, $table);
+	$this->assertTrue( $wpdb->get_var( "SELECT provider_id FROM $table WHERE id=3" ) == '4' );
     }
 
     function test_populate_buy() {
@@ -36,12 +57,26 @@ class InstallTest extends WP_UnitTestCase {
 	$this->assertTrue( $wpdb->get_var( "SELECT total_price FROM $table_name WHERE id='1'" ) == 4.5 );
     }
 
+    function test_populate_recipe() {
+	global $wpdb;
+	$table_name = $wpdb->prefix . "recipe";
+	populate_recipes($wpdb, $table_name);
+	$this->assertTrue( $wpdb->get_var( "SELECT difficulty FROM $table_name WHERE id='2'" ) == '1' );
+    }
+
+    function test_populate_recipe_tag() {
+	global $wpdb;
+	$table = $wpdb->prefix . 'recipe_tag';
+	populate_recipe_tags($wpdb, $table);
+	$this->assertTrue( $wpdb->get_var( "SELECT for_id FROM $table WHERE id=4" ) == '2' );
+    }
+    
 
     function test_uninstall() {
 	global $wpdb;
 
-	$price_table_name = $wpdb->prefix . "price";
+	$table = $wpdb->prefix . "buy";
 	myc_uninstall();
-	$this->assertTrue( $wpdb->query( "SHOW TABLES LIKE '$price_table_name'" ) === 0 );
+	$this->assertTrue( $wpdb->query( "SHOW TABLES LIKE '$table'" ) === 0 );
     }
 }
