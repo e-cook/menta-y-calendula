abb8d286153a3e97bb6d96fd0ba187a361a4d4f6
e-cook <>
2017-05-01 14:18:30 +0200

    activation and deactivation works

--- menta-y-calendula.php
+++ menta-y-calendula.php
@@ -1,31 +1,31 @@
 <?php
 /*
-Plugin Name: Menta y Calendula
-Plugin URI:  https://github.org/menta-y-calendula
-Description: Organic Food Preparation and Ordering
-Version:     20170927
-Author:      e-cook
-Author URI:  https://github.com/e-cook/menta-y-calendula
-License:     GPLv3
-License URI: https://www.gnu.org/licenses/gpl-3.0.html
-Text Domain: en
-Domain Path: /languages
+   Plugin Name: Menta y Calendula
+   Plugin URI:  https://github.org/menta-y-calendula
+   Description: Organic Food Preparation and Ordering
+   Version:     20170927
+   Author:      e-cook
+   Author URI:  https://github.com/e-cook/menta-y-calendula
+   License:     GPLv3
+   License URI: https://www.gnu.org/licenses/gpl-3.0.html
+   Text Domain: en
+   Domain Path: /languages
 
- This file is part of Menta y Calendula.
+   This file is part of Menta y Calendula.
 
-    Menta y Calendula is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+   Menta y Calendula is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
-    Menta y Calendula is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+   Menta y Calendula is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with Menta y Calendula.  If not, see <http://www.gnu.org/licenses/>.
-*/
+   You should have received a copy of the GNU General Public License
+   along with Menta y Calendula.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 defined( 'ABSPATH' ) or die();
 
@@ -54,26 +54,6 @@ function recipe_init() {
 add_action( 'init', 'recipe_init' );
 
 load_plugin_textdomain( 'menta-y-calendula', false, basename( dirname( __FILE__ ) ) . '/languages' );
-
-/*
-function upgrade_not_found($problematic_version_number) {
-    ?>
-    <div class="error">
-        <p><?php _e( "Could not find code to upgrade to version $problematic_version_number", 'myc-text-domain' ); ?></p>
-    </div>
-<?php
-}
-
-function register_version_number($new_version_float) {
-    global $myc_db_version;
-    global $myc_all_db_versions;
-    if( !in_array( $new_version_float, $myc_all_db_versions )) {
-	add_action( 'admin_notices', upgrade_not_found( $new_version_float ));
-    }
-    $myc_db_version = strval($new_version_float);
-    add_option( 'myc_db_version', $myc_db_version );
-}
-*/
 
 function myc_install_0_1() {
     global $wpdb;
@@ -115,20 +95,36 @@ KEY provider_id (provider_id)
 function myc_uninstall_0_1() {
     global $wpdb;
 
+    $posts_table = $wpdb->prefix . 'posts';
     $buy_table_name = $wpdb->prefix . "buy";
     $provided_by_table_name = $wpdb->prefix . 'provided_by';
 
     $wpdb->query("DROP TABLE IF EXISTS $buy_table_name");  
-    $wpdb->query("DROP TABLE IF EXISTS $provided_by_table_name");  
+    $wpdb->query("DROP TABLE IF EXISTS $provided_by_table_name");
+    $wpdb->query("DELETE FROM $posts_table WHERE id>9");
 }
 
 function float_version_to_string($version) {
     return str_replace( '.', '_', strval( $version ));
 }
 
-function myc_install( $myc_test_db_versions=array() ) {
+function myc_install() {
     myc_install_0_1();
     add_option( 'myc_db_version', '0.1', '', 'yes' );
+
+    global $wpdb;
+    if ('myc_'===$wpdb->prefix) {
+	require_once( dirname( __FILE__ ). '/tests/populate_database.php' );
+
+	$posts_table = $wpdb->prefix . 'posts';
+	populate_products($wpdb, $posts_table);
+
+	$provided_by_table = $wpdb->prefix . 'provided_by';
+	populate_providers($wpdb, $provided_by_table);
+
+	$table_name = $wpdb->prefix . "buy";
+	populate_buy($wpdb, $table_name);
+    }
     return 1;
 }
 
@@ -138,6 +134,6 @@ function myc_uninstall() {
     myc_uninstall_0_1();
 }
 
-register_activation_hook( __FILE__, 'myc_uninstall' );
+register_deactivation_hook( __FILE__, 'myc_uninstall' );
 
 //      fwrite(STDERR, print_r( "\nmyc_install calling " . $update_func . "\n" ));
