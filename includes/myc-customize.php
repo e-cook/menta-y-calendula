dc8cfff4396fa682c8ebba8ad4fbe85e263a9938
e-cook <e-cook@>
2017-08-08 15:39:13 +0200

    obsolete function

--- includes/myc-customize.php
+++ includes/myc-customize.php
@@ -83,7 +83,7 @@ add_filter( 'woocommerce_add_to_cart_handler', function( $type, $product ) {
 // show tags on shop page
 add_action( 'woocommerce_after_shop_loop_item', function() {
     global $product;
-    echo get_product_tag_list( $product->get_id(), ', ', '<span class="tagged_as">', '.</span>' );
+    echo wc_get_product_tag_list( $product->get_id(), ', ', '<span class="tagged_as">', '.</span>' );
 }, 5);
 
 /**
